a4c4f875304bada741b8fd652bbadf7af36062d7
unknown <nafim191119@gmai.com>
2024-02-26 19:26:14 +0600

    navbar updated

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
                 <div className="navbar-start">
                     <Link to="/"><a className="btn btn-ghost normal-case text-3xl text-white">MIST EECE-21</a></Link>
                 </div>
-                <div className="navbar-center hidden lg:flex text-white">
+                <div className="navbar-center lg:flex text-white">
                     <ul className="menu menu-horizontal px-1 font-bold">
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/profile">Profile</Link>  </li>
