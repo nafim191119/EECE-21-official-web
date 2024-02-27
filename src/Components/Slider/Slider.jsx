d1eb8e02866fc1cf49e287810e2f9d9f2cbedb3e
unknown <nafim191119@gmai.com>
2024-02-27 15:39:26 +0600

    picture updated

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,23 +1,25 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import img1 from ".//../../assets/banner1.png";
+import img2 from ".//../../assets/banner2.png";
+import img3 from ".//../../assets/banner3.png";
 
 const Slider = () => {
     return (
         <div>
             <Carousel>
                 <div>
-                    <img src="/src/assets/banner1.png" />
+                    <img src={img1} />
                     <p className="legend">Legend 1</p>
                 </div>
                 <div>
-                    <img src="/src/assets/banner2.png" />
+                    <img src={img2} />
                     <p className="legend">Legend 1</p>
                 </div>
                 <div>
-                    <img src="/src/assets/banner3.png" />
+                    <img src={img3} />
                     <p className="legend">Legend 1</p>
                 </div>
-                
             </Carousel>
         </div>
     );
